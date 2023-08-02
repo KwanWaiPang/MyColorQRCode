2ffb9a8baebc410ed47915f92afc053fe94d1f48
KwanWaiPang <gwpscut@163.com>
2023-08-02 22:55:37 +0800

    123456

---
+++ wechat-qrcode-scanning/src/main/java/com/king/wechat/qrcode/scanning/WeChatCameraScanFragment.java
@@ -0,0 +1,24 @@
+package com.king.wechat.qrcode.scanning;
+
+import com.king.mlkit.vision.camera.BaseCameraScanFragment;
+import com.king.mlkit.vision.camera.analyze.Analyzer;
+import com.king.wechat.qrcode.scanning.analyze.WeChatScanningAnalyzer;
+
+import java.util.List;
+
+import androidx.annotation.Nullable;
+
+/**
+ * 微信二维码扫描 - 相机扫描基类
+ * <p>
+ * 通过继承 {@link WeChatCameraScanActivity}或{@link WeChatCameraScanFragment}可快速实现二维码扫描
+ *
+ * @author <a href="mailto:jenly1314@gmail.com">Jenly</a>
+ */
+public abstract class WeChatCameraScanFragment extends BaseCameraScanFragment<List<String>> {
+    @Nullable
+    @Override
+    public Analyzer<List<String>> createAnalyzer() {
+        return new WeChatScanningAnalyzer();
+    }
+}
